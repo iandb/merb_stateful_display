75ae4a5802efd0c0347ff1f5228b12e8eb75d850
Shay Arnett <shayarnett@gmail.com>
2008-10-30 11:08:07 -0400

    skeleton

---
+++ lib/merb_stateful_display/merbtasks.rb
@@ -0,0 +1,6 @@
+namespace :merb_stateful_display do
+  desc "Do something for merb_stateful_display"
+  task :default do
+    puts "merb_stateful_display doesn't do anything"
+  end
+end
---
+++ spec/merb_stateful_display_spec.rb
@@ -0,0 +1,7 @@
+require File.dirname(__FILE__) + '/spec_helper'
+
+describe "merb_stateful_display" do
+  it "should do nothing" do
+    true.should == true
+  end
+end
